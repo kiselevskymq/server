e3d8259484709d6b8e89f91a16afc04e59c12b91
Макс Киселевский <kiselevsky.m@gmail.com>
2022-05-08 19:20:42 +0300

    .

--- index.ts
+++ index.ts
@@ -24,8 +24,10 @@ index.use(express.urlencoded({extended: false}));
 index.use(express.static('public'))
 index.use(helmet())
 index.use(cors({
-    origin: 'https://qwertyo-kiselevskym.vercel.app/',
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204
 }))
 
 
