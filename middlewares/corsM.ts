2457e86df4c62bded8f30a9b742103c547cc308a
Макс Киселевский <kiselevsky.m@gmail.com>
2022-05-09 00:08:41 +0300

    .

--- middlewares/corsM.ts
+++ middlewares/corsM.ts
@@ -1,7 +1,8 @@
 import {Response, Request, NextFunction} from 'express'
 
 export default function corsM(req:Request, res:Response, next:NextFunction) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+    res.setHeader("Access-Control-Allow-Origin", 'https://qwertyo-kiselevskym.vercel.app/');
+    res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS,PUT,DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Accept');
+    next()
 }
