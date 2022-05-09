3c72f1e46a5411588f2c6fa9abfa24f1a688c86d
Макс Киселевский <kiselevsky.m@gmail.com>
2022-05-09 08:12:07 +0300

    .

--- index.ts
+++ index.ts
@@ -20,8 +20,6 @@ const index: Express = express();
 const port = process.env.PORT || 3020;
 
 
-
-
 var corsOptions = {
     origin: '*',
     optionsSuccessStatus: 200,
@@ -38,21 +36,13 @@ index.use(helmet())
 
 index.use(function (req, res, next) {
 
-    // Website you wish to allow to connect
     res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     // @ts-ignore
     res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, OPTIONS, DELETE');
 
-    // Pass to next layer of middleware
+
     next();
 });
 
