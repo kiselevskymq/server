36810f153f85a5a09c103929f0856184d8d381a3
Макс Киселевский <kiselevsky.m@gmail.com>
2022-05-09 08:07:35 +0300

    .

--- index.ts
+++ index.ts
@@ -4,7 +4,6 @@ import mongoose from "mongoose";
 import router from "./routes";
 import verifyToken from "./middlewares/verifyToken";
 import helmet from "helmet";
-import corsM from "./middlewares/corsM";
 
 const cors = require('cors')
 
@@ -27,14 +26,35 @@ var corsOptions = {
     origin: '*',
     optionsSuccessStatus: 200,
 }
-index.options('*', cors())
-index.use(cors(corsOptions));
+
 
 index.use(express.json());
 index.use(express.urlencoded({extended: false}));
 index.use(express.static('public'))
 index.use(helmet())
 
+//index.options('*', cors())
+//index.use(cors(corsOptions));
+
+index.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    // @ts-ignore
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 index.use('/heroes', verifyToken, router);
 
